a01ea4ab24d8fc10aac2779bb29ffb107ce162c1
chandana-200766 <chandanamarripati6@gmail.com>
2026-01-31 16:12:33 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class user
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter age value");
+        int age=sc.nextInt();
+        System.out.println("enter b value:");
+        double b=sc.nextDouble();
+        System.out.println("enter str1 value:");
+        String str1=sc.next();
+        System.out.println("enter c value:");
+        float c=sc.nextFloat();
+        System.out.println("enter str2 value:");
+        String str2=sc.next();
+
+    }
+}
+
