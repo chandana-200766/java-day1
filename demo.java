0bb1ee552a8c89e9bc83762f6c1089c808e14f93
chandana-200766 <chandanamarripati6@gmail.com>
2026-01-31 14:29:26 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class demo {
+    public static void main(String args[]){
+        System.out.println("hello");
+        System.out.print("hai");
+        System.out.print("welcome");
+    }
+    
+}
