b87e8cb5c40842922664b6d06491cbfcb539f3ea
chandana-200766 <chandanamarripati6@gmail.com>
2026-02-07 14:22:08 +0530

    Add files via upload

---
+++ Operaters.java
@@ -0,0 +1,37 @@
+public class Operaters 
+{
+    public static void main(String[]args)
+    {
+        int a=5;
+        int b=6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("Relational Operaters");
+        System.out.println(a==5);
+        System.out.println(a!=5);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println("unary Operator");
+        System.out.println(+a);
+        System.out.println(-a);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println("Assignment Operaters");
+        System.out.println(a+=5);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+
+
+        
+    }
+    
+}
