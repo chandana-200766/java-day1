e9ce4b24b814e7375e29437cecf5a75f583fec10
chandana-200766 <chandanamarripati6@gmail.com>
2026-02-07 15:35:29 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,19 @@
+public class Swapping {
+    public static void main(String[]args){
+        int a=5;
+        int b=9;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+        int c=5;
+        int d=6;
+        c=c*d;
+        d=c/d;
+        c=c/d;
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
