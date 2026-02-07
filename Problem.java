617024c07d78a6536fe752f7609dcc709eea9dd1
chandana-200766 <chandanamarripati6@gmail.com>
2026-02-07 15:05:49 +0530

    Add files via upload

---
+++ Problem.java
@@ -0,0 +1,31 @@
+public class Problem {
+    public static void main(String[] args) 
+    {
+        int a=8;
+        int b=6;
+        System.out.println(a+b);
+        if(a<0)
+        {
+            System.out.println("number is positive");
+        }
+        else
+        {
+            System.out.println("number is negative");
+        }
+        if(a<0 && a==8);
+        {
+            System.out.println("positive");
+        }
+        if(a%2==0 ||a==9)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("not even");
+        }
+        System.out.println(++a);
+        
+    }
+    
+}
