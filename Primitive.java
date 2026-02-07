145b1f8616883eecbfbd7acc8796f862c77c3868
chandana-200766 <chandanamarripati6@gmail.com>
2026-02-07 13:27:54 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,15 @@
+public class Primitive 
+{
+    public static void main(String[]args)
+    {
+        int te=99;
+        int en=86;
+        int hi=77;
+        int sum=(te+en+hi);
+        int avg=sum/3;
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+    
+}
+    
